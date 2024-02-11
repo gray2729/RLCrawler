9dd7f4a9c273b1d5dcc3fc994ac403fab41f7fcf
isaac gray <igray2729@gmail.com>
2024-02-10 16:59:08 -0800

    modified movements and added random walk

--- CrawlerMove.py
+++ CrawlerMove.py
@@ -1,25 +1,24 @@
 from picrawler import Picrawler
 from vilib import Vilib
-from robot_hat import Music, TTS
 import random
 
 crawler = Picrawler([10,11,12,4,5,6,1,2,3,7,8,9])
 
 #Move Forward
 def mForward(speed):
-    crawler.do_action('forward',3,speed)
+    crawler.do_action('forward',1,speed)
     
 #Move Right
 def mRight(speed):
-    crawler.do_action('turn right',3,speed)
+    crawler.do_action('turn right',1,speed)
     
 #Move Left
 def mLeft(speed):
-    crawler.do_action('turn left', 3,speed)
+    crawler.do_action('turn left',1,speed)
     
 #Move Back
 def mBack(speed):
-    crawler.do_action('backward',3,speed)
+    crawler.do_action('backward',1,speed)
     
 #Test Sensors
 def mTestSensor(speed):
@@ -28,35 +27,43 @@ def mTestSensor(speed):
     while Vilib.detect_obj_parameter['color_n']!=1:
         mRight(speed)
         
-    Music.sound_effect_play('./sounds/sign.wav')
-        
+    print("Finished") 
     
+#Random Walk
+def mRandomWalk(speed):
+    numActions = 0
+    
+    Vilib.color_detect("blue")
+    while Vilib.detect_obj_parameter['color_n']!=1:
+        randN = random.randint = (1, 100)
+        
+        if randN < 33:
+            mRight(speed)
+        elif randN < 66:
+            mLeft(speed)
+        else:
+            mForward(speed)
+            
+        numActions += 1
+            
+        Vilib.color_detect("red")
+        if Vilib.detect_obj_parameter['color_n']!=1:
+            mBack(speed)
+            
+        Vilib.color_detect("blue")
+    
+    print("Finished Random Walk")
+    return numActions
 
 if __name__ == '__main__':
     Vilib.camera_start()
     Vilib.display()
     
-    Music.music_set_volume(20)
-    TTS.lang("en-US")
-    
     speed = 100
     
-    mTestSensor(speed)
+    actions = mRandomWalk(speed)
+    print("Total number of actions: ", actions)
     
-    #while Vilib.detect_obj_parameter['color_n']!=1:
-     #   randN = random.randint(1, 100)
-    
-     #   if randN < 30:
-     #       mForward(speed)
-            
-     #   elif randN < 50:
-     #       mRight(speed)
-            
-     #   elif randN < 70:
-     #       mLeft(speed)
-            
-     #   else:
-     #       mBack(speed)
         
         
             
